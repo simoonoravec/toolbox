7fdf895b024841fe383dcfe9a7cdddf464818c6e
Simon Oravec <simon@0r4v3c.xyz>
2023-09-25 17:07:48 +0200

    Update js-bundler

--- js-bundler/bundler.py
+++ js-bundler/bundler.py
@@ -6,6 +6,11 @@ js_files = [f for f in os.listdir('./src') if f.endswith('.js')]
 # Sort the files alphabetically to ensure consistent order
 js_files.sort()
 
+def removeSourceMapping(string):
+    lines = string.splitlines()
+    lines_filtered = [line for line in lines if not line.startswith("//# sourceMappingURL=")]
+    return '\n'.join(lines_filtered)
+
 # Combine and the files
 with open('./jsbundle.min.js', 'w') as outfile:
     for js_file in js_files:
@@ -13,7 +18,7 @@ with open('./jsbundle.min.js', 'w') as outfile:
         
         with open(file_path, 'r') as infile:
             content = infile.read()
-            outfile.write(content)
+            outfile.write(removeSourceMapping(content));
             outfile.write('\n\n')
 
 print(f'Combined {len(js_files)} JavaScript files into jsbundle.min.js')
